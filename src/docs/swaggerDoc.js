e77216c473676c89e14907720ac52cb2c3b172b8
BryanVillabona <bvillabona13@gmail.com>
2025-10-15 19:29:39 -0500

    feat: :bug: Correccion documentacion API
    By @BryanVillabona

--- src/docs/swaggerDoc.js
+++ src/docs/swaggerDoc.js
@@ -1,7 +1,3 @@
-import 'dotenv/config';
-
-const RENDER_URL = process.env.RENDER_URL || `http://localhost:${process.env.PORT}`;
-
 export const swaggerDocument = {
   openapi: "3.0.0",
   info: {
@@ -11,12 +7,8 @@ export const swaggerDocument = {
   },
   servers: [
     {
-      url: RENDER_URL,
-      description: "Servidor de Producción"
-    },
-    {
-      url: `http://localhost:${process.env.PORT}`,
-      description: "Servidor Local de Desarrollo"
+      url: "http://localhost:4000",
+      description: "Servidor local de desarrollo"
     }
   ],
   paths: {
